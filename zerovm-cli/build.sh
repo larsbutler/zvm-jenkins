d85ccba6d5a7d2eaee6db52cd08750e18ea9aff7
Lars Butler <Lars.Butler@gmail.com>
2014-03-05 18:54:40 +0000

    zerovm-cli: fix misc bugs in zerovm-cli scripts
    There were a bunch of errors, including copy/paste errors and arg
    quotation errors.

--- zerovm-cli/build.sh
+++ zerovm-cli/build.sh
@@ -5,9 +5,10 @@ set -e
 export WORKSPACE=$HOME/zerovm-cli
 export GITURL=$1
 export BRANCH=$2
+DEPS="python-pip git python3 devscripts debhelper"
 
 sudo apt-get update
-sudo apt-get install --yes --force-yes python-pip
+sudo apt-get install --yes --force-yes $DEPS
 sudo pip install tox
 
 # Clone and build
