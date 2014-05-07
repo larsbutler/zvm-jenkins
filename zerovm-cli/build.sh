e23681bbbd0e11f4ea6c15ff4a475e4821bc8103
Lars Butler <Lars.Butler@gmail.com>
2014-05-07 19:08:53 +0200

    zerovm-cli/build.sh: install non-std py3 versions
    Python 3.2, 3.2, and 3.4 are required for tests. This changes installs
    them inside the lxc container.

--- zerovm-cli/build.sh
+++ zerovm-cli/build.sh
@@ -7,7 +7,10 @@ export GITURL=$1
 export BRANCH=$2
 
 sudo apt-get update
-sudo apt-get install --yes --force-yes python-pip python3
+sudo apt-get install --yes --force-yes python-software-properties
+# install non-standard python versions:
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get install --yes --force-yes python-pip python3.2 python3.3 python3.4
 sudo pip install tox
 
 git clone $GITURL/zerovm-cli.git $WORKSPACE
